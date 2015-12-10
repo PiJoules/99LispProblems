8845c7bf7614a02546c9d4f40c0bde13d77c25ed
Leo is awesome <lchan1994@yahoo.com>
2015-12-09 21:07:06 -0500

    gotta increase that github score

--- prob9.lisp
+++ prob9.lisp
@@ -1,43 +1,39 @@
-(defun pack(l)
-	; call a function that looks up
-	; the first elements of the list
-	; until it reaches a different element
-	; and append this element to a new list
-	;
-	; this new list will be the old list
-	; with the first same elements trimmed
+(defun pack (l)
+	; Append the trimmed group of elems in the head
+	; to the trimmed rest of the body gone through
+	; pack.
 	(if (null l)
 		nil
-		(cons (same_first l) (pack (trimmed_first l)))
+		(cons (trimmed-head l) (pack (trimmed-rest l)))
 	)
 )
 
-(defun same_first (l)
-	; The first element should be the same as
-	; the second element to get appended to
-	; the sublist.
-	(cond
-		((null l) nil)  ; nothing
-		((null (rest l)) l)  ; one elem left in list
-		((equal (first l) (second l))
-			; first and second elem are same
-			(cons (first l) (same_first (rest l)))
+(defun trimmed-head (l)
+	; Retain first elems up to different second
+	(if (null l)
+		nil
+		(let ((head (first l)) (next (second l)))
+			; head is elem, next is nil
+			; head and next are same
+			; head and next are diff
+			(cond
+				((eq next nil) (list head))
+				((eq head next) (cons head (trimmed-head (rest l))))
+				(t (list head))
+			)
 		)
-		(t (list (first l)))  ; they are different, so return a list of the first elem
 	)
 )
 
-(defun trimmed_first (l)
-	; Return the list with the first same elems
-	; trimmed from it.
-	; Just keep trimming while the first and
-	; second elem are the same
-	(cond
-		((null l) nil)
-		((null (rest l)) nil)
-		((equal (first l) (second l))
-			(trimmed_first (rest l))
+(defun trimmed-rest (l)
+	(if (null l)
+		nil
+		(let ((head (first l)) (next (second l)))
+			(cond
+				((eq next nil) (rest l))
+				((eq head next) (trimmed-rest (rest l)))
+				(t (rest l))
+			)
 		)
-		(t (rest l))
 	)
 )
